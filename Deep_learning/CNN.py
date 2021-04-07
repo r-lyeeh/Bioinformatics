8017aa9403dba0287b3dab891e5afa842993fc6b
R'lyeh <343282676@qq.com>
2021-04-07 10:54:24 +0200

    Add files via upload

---
+++ Deep_learning/CNN.py
@@ -0,0 +1,69 @@
+# import libraries
+import os 
+import keras 
+from keras.models import Sequential 
+from keras.layers import Dense, Dropout, Flatten 
+from keras.layers import Conv2D, MaxPooling2D 
+from keras.utils import to_categorical 
+from keras.preprocessing import image
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from tqdm import tqdm
+import pickle
+
+# data reading
+os.chdir("/Users/pang/Python_project/")
+train = pd.read_csv('train.csv')
+# We have grayscale images, so while loading the images we will keep grayscale=True
+# if you have RGB images, you should set grayscale as False
+train_image = []
+for i in tqdm(range(train.shape[0])):
+    img = image.load_img('train/'+train['id'][i].astype('str')+'.png', target_size=(28,28,1), grayscale=True)
+    img = image.img_to_array(img)
+    img = img/255
+    train_image.append(img)
+    X = np.array(train_image)
+# multi class; one-hot coding
+y=train['label'].values
+y = to_categorical(y)
+
+# cross-validation
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
+# define structure: 1 hidden layer; 1 output layer
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(28,28,1)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['accuracy'])
+# train
+model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+
+# save model
+with open('model.pkl','wb') as f:
+    pickle.dump(model,f)
+# show all variables
+# dir()
+# prediction
+test = pd.read_csv('test.csv')
+test_image = []
+for i in tqdm(range(test.shape[0])):
+    img = image.load_img('test/'+test['id'][i].astype('str')+'.png', target_size=(28,28,1), grayscale=True)
+    img = image.img_to_array(img)
+    img = img/255
+    test_image.append(img)
+test = np.array(test_image)
+# making predictions
+prediction = model.predict_classes(test)
+
+# creating submission file
+sample = pd.read_csv('sample_submission_I5njJSF.csv')
+sample['label'] = prediction
+sample.to_csv('sample_cnn.csv', header=True, index=False)
